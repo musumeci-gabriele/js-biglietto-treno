b44c2ca34adbd45144b0964d056a1c8833affbc6
Gabriele-Musumeci <musumeci.gabriele@hotmail.com>
2020-08-05 11:00:06 +0200

    controllo

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 // INSERIMENTO ETA'
 var eta = prompt("Quanti anni hai ?");
 
+// CONTROLLO INSERIMENTO
+if (isNaN(eta)) {
+  alert("Inserisci un numero !")
+  var eta = prompt("Quanti anni hai ?");
+}
+
 // INSERIMENTO KM
 var km = prompt("Quanti km vuoi percorrere ?");
+
+// CONTROLLO INSERIMENTO
+if (isNaN(km)) {
+  alert("Inserisci un numero !")
+  var eta = prompt("Quanti km vuoi percorrere ?");
+}
+
 
 // PREZZO BIGLIETTO 0,21 CEN / KM
 var prezzo = (km * 0.21);
@@ -11,14 +24,11 @@ var prezzo = (km * 0.21);
 if (eta<18){
   var prezzoMin = (prezzo - ((prezzo / 100)* 20));
   document.getElementById("prezzo-bit").innerHTML = prezzoMin;
-
 }
 else if (eta>65){
   var prezzoOver = (prezzo - ((prezzo / 100)* 40));
   document.getElementById("prezzo-bit").innerHTML = prezzoOver;
-
 }
 else {
   document.getElementById("prezzo-bit").innerHTML = prezzo;
-
 }
